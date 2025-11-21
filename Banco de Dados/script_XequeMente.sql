3186277e1a3143bb3851f3065f21adb717355ec3
ViniiGama <vinicius.gama@sptech.school>
2025-11-21 09:47:30 -0300

    Alterações no script e modelagem do banco de dados

--- Banco de Dados/script_XequeMente.sql
+++ Banco de Dados/script_XequeMente.sql
@@ -5,5 +5,28 @@ CREATE TABLE usuario(
 idUsuario INT PRIMARY KEY AUTO_INCREMENT,
 nome VARCHAR(45),
 email VARCHAR(100),
-senha VARCHAR(45)
+senha VARCHAR(45),
+experiencia INT
 );
+
+CREATE TABLE quiz(
+idQuiz INT PRIMARY KEY AUTO_INCREMENT,
+nome VARCHAR(45),
+descricao VARCHAR(100),
+pergunta VARCHAR(100)
+);
+
+CREATE TABLE tentativa(
+idTentativa INT,
+fkUsuario INT,
+fkQuiz INT,
+PRIMARY KEY (idTentativa, fkUsuario, fkQuiz),
+CONSTRAINT fkUsuarioTentativa
+	FOREIGN KEY (fkUsuario)
+		REFERENCES usuario(idUsuario),
+CONSTRAINT fkQuizTentativa
+	FOREIGN KEY (fkQuiz)
+		REFERENCES quiz(idQuiz)
+);
+
+
